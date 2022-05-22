54c12c32341f8856a88dc8dc029f71b1c5cc8a67
LindaDaugule <Bieza777@inbox.lv>
2022-05-22 18:30:17 +0300

    CenteredAverage

--- Exercises/Level1/Exercises.Level1/Array2.cs
+++ Exercises/Level1/Exercises.Level1/Array2.cs
@@ -60,7 +60,23 @@ public class Array2
     /// </summary>
     public int CenteredAverage(int[] nums)
     {
-        throw new NotImplementedException();
+        int sum = 0;
+        int largest = nums[0];
+        int smallest = nums[0];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+            smallest = Math.Min(smallest, nums[i]);
+            largest = Math.Max(largest, nums[i]);
+
+            sum = sum + nums[i];
+        }
+
+        sum = sum - largest;
+        sum = sum - smallest;
+
+        return sum / (nums.Length - 2);
     }
 
     /// <summary>
